4466732258b737e8e4da53071166a1692e179f09
Saad Shahd <dev.saad.shahd@gmail.com>
2019-11-10 03:48:44 +0200

    Add charge, refund and status requests

--- index.js
+++ index.js
@@ -1,9 +1,78 @@
 'use strict';
 
-module.exports = (input, {postfix = 'rainbows'} = {}) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+const crypto = require('crypto');
+const axios = require('axios');
 
-	return `${input} & ${postfix}`;
+const API_PATH = '/ECommerceWeb/Fawry/payments/';
+const BASE_URL = 'https://www.atfawry.com';
+const SANDBOX_BASE_URL = 'https://atfawry.fawrystaging.com';
+
+const getUrl = isSandbox => (isSandbox ? SANDBOX_BASE_URL : BASE_URL) + API_PATH;
+const formatAmount = amount => amount.toFixed(2);
+const getSignature = (...strings) => {
+	return crypto
+		.createHash('sha256')
+		.update(strings.join(strings, ''))
+		.digest('hex');
+};
+
+const init = ({
+	fawrySecureKey,
+	isSandbox = false
+} = {}) => {
+	const request = axios.create({
+		baseURL: getUrl(isSandbox)
+	});
+
+	return {
+		request,
+		charge: data => {
+			const signature = getSignature(
+				data.merchantCode,
+				data.merchantRefNum,
+				data.customerProfileId,
+				data.paymentMethod,
+				formatAmount(data.amount),
+				data.cardToken || '',
+				fawrySecureKey
+			);
+
+			return request.post('charge', {
+				...data,
+				signature
+			});
+		},
+
+		refund: data => {
+			const signature = getSignature(
+				data.merchantCode,
+				data.referenceNumber,
+				formatAmount(data.refundAmount),
+				data.reason,
+				fawrySecureKey
+			);
+
+			return request.post('refund', {
+				...data,
+				signature
+			});
+		},
+
+		status: params => {
+			const signature = getSignature(
+				params.merchantCode,
+				params.merchantRefNumber,
+				fawrySecureKey
+			);
+
+			return request.get('status', {
+				...params,
+				signature
+			});
+		}
+	};
+};
+
+module.exports = {
+	init
 };
